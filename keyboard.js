d0aeb7c537898e551d7b1ee83ae4b74c9e292c9b
Claire Bergman <cbergman6@gatech.edu>
2015-11-16 15:22:21 -0500

    Added in visual feedback for typing words and backspace

--- keyboard.js
+++ keyboard.js
@@ -3,26 +3,39 @@
 var curStr = ''
 var ENTER = 13;
 var DELETE = 46;
+var BSPACE = 8;
+var curHTMLStr = document.getElementById('curStr');
+
+curHTMLStr.style.width = 100;
+curHTMLStr.style.height = 100;
+
 
 //Listen for key down
 document.addEventListener('keydown', function(event) {
-	//If enter, add word to plot, empty word string
-	if (event.keyCode == ENTER){
-		console.log(curStr);
-		addWord(curStr);
-		curStr = '';
-	}
 
-	//if delete, delte selected object
-	else if(event.keyCode==DELETE){
-		if(lastInd!=null){
+	switch(event.keyCode){
+		case ENTER: //If enter, add word to graph
+			console.log(curStr);
+			addWord(curStr);
+			curStr = '';
+			break;
+		case DELETE: //If delete, delete selected word.
+			if(lastInd!=null){
 			 removeWord(lastInd);
 			 lastInd = null;
-		}
+			}
+			break;
+		case BSPACE: //if backspace, remove last character from word
+			curStr = curStr.substring(0,curStr.length-1);
+			break;
+		default: //Otherwise, add character to plot
+			if (event.which != null){
+				curStr = curStr + String.fromCharCode(event.keyCode);
+			}
+
 	}
 
-	//Otherwise, assume letter, add to word string. 
-	else if (event.which != null){
-		curStr = curStr + String.fromCharCode(event.keyCode);
-	}
+	curHTMLStr.innerHTML = curStr;
+
+	
 });
