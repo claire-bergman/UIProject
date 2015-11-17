f063cb99a100582b790d75863eb4babbc2556261
Claire Bergman <cbergman6@gatech.edu>
2015-11-17 06:56:10 -0500

    Added colors

--- src/buttonUtil.js
+++ src/buttonUtil.js
@@ -1,12 +1,17 @@
 var sentences = [];
 
 
-//adds a linear sentence
+//adds a linear sentence based on order items were added
 function addSentence(){
+	var curColor = new THREE.Color('rgb('
+		+ Math.round(Math.random()*255) + ',' 			
+		+ Math.round(Math.random()*255) + ',' 			
+		+ Math.round(Math.random()*255) + ')');	
+
 	console.log("sentence");
 	var curSentence = [];
 	for (var i=1; i<selectedNodes.length; i++){
-		curSentence.push(addLine(words[selectedNodes[i-1]],words[selectedNodes[i]]));
+		curSentence.push(addLine(words[selectedNodes[i-1]],words[selectedNodes[i]], curColor));
 	}
 }
 
@@ -17,14 +22,14 @@ function addSentence(){
 
 
 // Helper function to add a line from one node-word to another
-function addLine(word1, word2) {
+function addLine(word1, word2, color) {
 
 	var axisGeo = new THREE.Geometry()
 	axisGeo.vertices.push(
 		word1.coordinates, word2.coordinates
 	)
 	var axisMat = new THREE.LineBasicMaterial({
-		color: 0xeeeeee
+		color: color
 	})
 	var axes = new THREE.LineSegments(axisGeo, axisMat)
 	axes.type = THREE.Lines
